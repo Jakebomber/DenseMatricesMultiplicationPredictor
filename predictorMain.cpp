f4093b0468b5eb193b918d5bc931582126046b65
Jake Bomber <jakebomber@live.cn>
2021-02-16 16:47:28 +0800

    1

--- predictorMain.cpp
+++ predictorMain.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 vector<int> matricesDimensionsData;
-vector<string> matricesBrackets;
+//vector<string> matricesBrackets;
 int p = 0;
 map<int, map<int, int> > a;
 long long int predict(vector<int> mdd, int i_start, int i_end,int status) {
@@ -19,9 +19,9 @@ long long int predict(vector<int> mdd, int i_start, int i_end,int status) {
 			p_min = i;
 		}
 	}
-	matricesBrackets[i_start - 1] += "(";
-	matricesBrackets[p_min] += ")(";
-	matricesBrackets[i_end] += ")";
+	//matricesBrackets[i_start - 1] += "(";
+	//matricesBrackets[p_min] += ")(";
+	//matricesBrackets[i_end] += ")";
 	a[i_start][i_end] = min;
 	return min;
 }
@@ -29,15 +29,17 @@ int main() {
 	//mtg:matrices group file
 	FILE* s1;
 	freopen_s(&s1,"data.mtg", "r", stdin);
+	if (s1 == NULL) {
+		cout << "文件打开失败！" << endl;
+		return 0;
+	}
 	int n;
-	while (cin >> n)matricesDimensionsData.push_back(n), matricesBrackets.push_back("");
+	//while (cin >> n)matricesDimensionsData.push_back(n), matricesBrackets.push_back("");
 	for (auto x : matricesDimensionsData)cout << x << endl;
 	cout << predict(matricesDimensionsData, 1, matricesDimensionsData.size() - 1,0);
-	cout << endl << matricesBrackets[0];
-	for (int i = 1; i < matricesDimensionsData.size(); i++) {
-		cout << "A" << i << matricesBrackets[i];
-	}
-	//sync!1
-
+	//cout << endl << matricesBrackets[0];
+	//for (int i = 1; i < matricesDimensionsData.size(); i++) {
+	//	cout << "A" << i << matricesBrackets[i];
+	//}
 	fclose(s1);
 }
