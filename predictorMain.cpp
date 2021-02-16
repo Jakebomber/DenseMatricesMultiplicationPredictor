8a8bca91bb0f2da6b843fd8f5ad51ce406a0ed9d
Jake Bomber <jakebomber@live.cn>
2021-02-16 17:13:52 +0800

    1

--- predictorMain.cpp
+++ predictorMain.cpp
@@ -19,9 +19,9 @@ long long int predict(vector<int> mdd, int i_start, int i_end,int status) {
 			p_min = i;
 		}
 	}
-	//matricesBrackets[i_start - 1] += "(";
-	//matricesBrackets[p_min] += ")(";
-	//matricesBrackets[i_end] += ")";
+	matricesBrackets[i_start - 1] += "(";
+	matricesBrackets[p_min] += ")(";
+	matricesBrackets[i_end] += ")";
 	a[i_start][i_end] = min;
 	return min;
 }
@@ -37,10 +37,10 @@ int main() {
 	while (cin >> n)matricesDimensionsData.push_back(n), matricesBrackets.push_back("");
 	for (auto x : matricesDimensionsData)cout << x << endl;
 	cout << predict(matricesDimensionsData, 1, matricesDimensionsData.size() - 1,0);
-	//cout << endl << matricesBrackets[0];
-	//for (int i = 1; i < matricesDimensionsData.size(); i++) {
-	//	cout << "A" << i << matricesBrackets[i];
-	//}
+	cout << endl << matricesBrackets[0];
+	for (int i = 1; i < matricesDimensionsData.size(); i++) {
+		cout << "A" << i << matricesBrackets[i];
+	}
 
 	fclose(s1);
 }
