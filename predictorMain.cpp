07ea092841937706d651b080c7cf2447bdb1eece
Jakebomber <jakebomber@live.cn>
2021-02-17 20:15:48 +0800

    优化了单对括号

--- predictorMain.cpp
+++ predictorMain.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <algorithm>
 using namespace std;
 vector<int> matricesDimensionsData;
 vector<string> matricesBrackets;
-int p = 0;
+int p = 0,*q;
 map<int, map<int, int> > a;
+bool cmp(char a, char b) {
+	return a > b;
+}
+void displayResult() {
+	cout << endl << matricesBrackets[0];
+	for (int i = 1; i < *q; i++) {
+		cout << "A" << i << matricesBrackets[i];
+	}
+	cout << endl;
+}
 long long int predict(vector<int> mdd, int i_start, int i_end,int status) {
 	if (a[i_start][i_end] != 0)return a[i_start][i_end];
 	if (i_start == i_end) return 0;
@@ -28,6 +39,7 @@ long long int predict(vector<int> mdd, int i_start, int i_end,int status) {
 int main() {
 	//mtg:matrices group file
 	FILE* s1;
+	q = new int();
 	freopen_s(&s1,"data.mtg", "r", stdin);
 	if (s1 == NULL) {
 		cout << "文件打开失败！" << endl;
@@ -37,10 +49,48 @@ int main() {
 	while (cin >> n)matricesDimensionsData.push_back(n), matricesBrackets.push_back("");
 	for (auto x : matricesDimensionsData)cout << x << endl;
 	cout << predict(matricesDimensionsData, 1, matricesDimensionsData.size() - 1,0);
-	cout << endl << matricesBrackets[0];
-	for (int i = 1; i < matricesDimensionsData.size(); i++) {
-		cout << "A" << i << matricesBrackets[i];
+	*q = matricesDimensionsData.size();
+
+	for (int i = 1; i < matricesBrackets.size(); i++) {
+		int ll = 0, rr = 0, l = 0, r = 0;
+		for (auto x : matricesBrackets[i - 1]) {
+			if (x == '(')ll++;
+			else rr++;
+		}
+		for (auto x : matricesBrackets[i]) {
+			if (x == '(')l++;
+			else r++;
+		}
+		if (ll <= r) {
+			r -= ll;
+			ll = 0;
+		}
+		else {
+			ll -= r;
+			r = 0;
+		}
+		matricesBrackets[i - 1] = "";
+		for (int j = 0; j < rr; j++) {
+			matricesBrackets[i - 1] += ')';
+		}
+		for (int j = 0; j < ll; j++) {
+			matricesBrackets[i - 1] += '(';
+		}
+		matricesBrackets[i] = "";
+		for (int j = 0; j < r; j++) {
+			matricesBrackets[i] += ')';
+		}
+		for (int j = 0; j < l; j++) {
+			matricesBrackets[i] += '(';
+		}
+		displayResult();
 	}
 
+	//for (auto &x : matricesBrackets) {
+	//	sort(x.begin(), x.end(),cmp);
+	//}
+
+
+	delete q;
 	fclose(s1);
 }
